660cad655fa6bbce1190eeb5550b035cbf4a7570
Yuta <jp.yuta0131@gmail.com>
2024-11-03 14:02:15 +0900

    cocからbuiltin lspに変えました

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,11 +1,53 @@
 require("config.lazy")
 
 require("ibl").setup()
-require('Comment').setup()
-require("nvim-tree").setup()
+require("Comment").setup()
+require("neo-tree").setup({
+  filesystem = {
+    hijack_netrw_behavior = "open_default",
+    filtered_items = {
+      visible = true,
+    }
+  },
+})
+
+require("mason").setup()
+require("mason-lspconfig").setup()
+require("mason-lspconfig").setup_handlers {
+  function(server_name)
+    require("lspconfig")[server_name].setup {}
+  end,
+}
+
+capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+vim.opt.completeopt = "menu,menuone,noselect"
+
+local cmp = require("cmp")
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      require("luasnip").lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
+    ["<C-n>"] = cmp.mapping.select_next_item(),
+    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.close(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+  }),
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
+  }, {
+    { name = "buffer" },
+  })
+})
 
 local autocmd = vim.api.nvim_create_autocmd
-
 autocmd("TermOpen", {
   pattern = "*",
   callback = function ()
@@ -18,12 +60,13 @@ vim.opt.shiftwidth = 2
 vim.opt.number = true
 vim.opt.mouse = ""
 vim.opt.encoding = "utf-8"
+vim.opt.title = true
 vim.cmd("colorscheme nordfox")
 
 local set = vim.keymap.set
 
-set("n", "<C-n>", ":NvimTreeOpen <CR>")
-
+set("n", "<C-n>", ":Neotree <CR>")
+set("n", "<C-w><C-t>C-t>", ":split + term <CR>")
 set("n", "<Up>", "<Nop>")
 set("n", "<Donw>", "<Nop>")
 set("n", "<Left>", "<Nop>")
--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -1,12 +1,32 @@
 return {
-  "dinhhuy258/git.nvim",
-  "norcalli/nvim-colorizer.lua",
-  "lukas-reineke/indent-blankline.nvim",
-  "numToStr/Comment.nvim",
-  "EdenEast/nightfox.nvim",
-  "nvim-tree/nvim-tree.lua",
+  { "dinhhuy258/git.nvim" },
+  { "norcalli/nvim-colorizer.lua" },
+  { "lukas-reineke/indent-blankline.nvim" },
+  { "numToStr/Comment.nvim" },
+  { "EdenEast/nightfox.nvim" },
+  
+  -- lsp --
+  { "neovim/nvim-lspconfig" },
+  { "williamboman/mason.nvim" },
+  { "williamboman/mason-lspconfig.nvim" },
+  
+  -- cmp --
+  { "L3MON4D3/LuaSnip" },
+  { "hrsh7th/nvim-cmp" },
+  { "hrsh7th/cmp-nvim-lsp" },
+  { "hrsh7th/cmp-buffer" },
+  { "saadparwaiz1/cmp_luasnip" },
+  
+  -- formater linter --
+  { "nvimtools/none-ls.nvim" },
+
   {
-    "neoclide/coc.nvim",
-    branch = "release",
-  },
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    }
+  }
 }
