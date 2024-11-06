1197b73f6e65b1346ca478bf1570a74a04df0516
Yuta <jp.yuta0131@gmail.com>
2024-11-07 08:02:55 +0900

    update

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -6,13 +6,14 @@ config.font_size = 10.0
 config.use_ime = true
 config.color_scheme = 'nightfox'
 config.enable_tab_bar = false
+config.window_background_opacity = 0.85
 
-config.window_padding = {
-  left = '0',
-  right = '0',
-  top = '0',
-  bottom = '0',
-}
+-- config.window_padding = {
+--   left = '0',
+  -- right = '0',
+  -- top = '0',
+--   bottom = '0',
+-- }
 
 config.font = wezterm.font(
   'Hack Nerd Font',
