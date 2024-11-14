b9b382f75fbd8cff10a1fae3329ce0d8496b844a
Yuta <jp.yuta0131@gmail.com>
2024-11-14 16:06:37 +0900

    update

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
+install() {
+  sudo pacman -S --needed $@
+}
 
+<<<<<<< Updated upstream
 # sudo pacman -S --needed 
+=======
+install base
+install base-devel
+install nodejs npm yarn pnpm
+install python
+install rustup
+install zsh zsh-completions zsh-syntax-highlighting zsh-autosuggestions
+install tmux
+install vim neovim
+install curl wget
+install git github-cli
+install fastfetch
+
+rustup toolchain install stable
+sudo chsh yuta -s /bin/zsh
+>>>>>>> Stashed changes
