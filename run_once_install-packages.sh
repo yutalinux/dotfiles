eee05d7213bc648c31f3b29159a90cba8cc51598
Yuta <jp.yuta0131@gmail.com>
2024-11-26 12:12:44 +0900

    update: インストールするパッケージを変更

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -3,17 +3,22 @@ install() {
 	sudo pacman -S --needed --noconfirm $@
 }
 
-install base \
-	base-devel \
-	nodejs npm yarn pnpm \
-	python \
-	rustup \
+install \
+	base \
+	base-devel cmake \
+	nodejs npm yarn pnpm deno \
+	python rustup go \
 	zsh zsh-completions zsh-syntax-highlighting zsh-autosuggestions \
 	tmux \
 	vim neovim \
-	curl wget \
-	git github-cli \
-	fastfetch
+	curl wget rsync openssh \
+	man-db \
+	man-pages \
+	git github-cli lazygit \
+	fastfetch bat
 
 rustup toolchain install stable
+npm install -g bun
 sudo chsh yuta -s /bin/zsh
+
+# paru -S numbat-bin
